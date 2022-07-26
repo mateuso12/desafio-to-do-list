b2f56343383bad6677bb0bbe3a7211e11fac8c74
Mateus <mateuso12@hotmail.com.br>
2022-07-25 21:41:31 -0300

    Configurações iniciais⚙️

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         'gray-100': '#F2F2F2',
         'gray-200': '#D9D9D9',
-        'gray-300': '#808080',
-        'gray-400': '#333333',
-        'gray-500': '#262626',
-        'gray-600': '#1A1A1A',
-        'gray-700': '#0D0D0D',
+        'blue-300': '#337def',
+        'gray-400': '#808080',
+        'blue-500': '#001629',
+        'blue-600': '#262626',
+        'blue-700': '#002647',
         'purple-200': '#8284fa',
         'purple-500': '#5E60CE',
         'blue-200': '#4EA8DE',
@@ -18,10 +21,14 @@ module.exports = {
         'red-500': '#E25858'
       }
     },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    screens: {
+      md:{ 'min':"736px", 'max': '1023px'},
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
   ],
 }
